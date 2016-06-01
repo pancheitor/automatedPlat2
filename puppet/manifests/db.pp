9c7b9f56f75746a1e5e8ab4a1dc6e669456a6267
root <root@principalhost>
2016-06-01 08:22:58 +0000

    Load balancer, websever y db funcionando OK. Falta el error del storage ya que la clase no permite usar el parametro force.

--- puppet/manifests/db.pp
+++ puppet/manifests/db.pp
@@ -3,26 +3,43 @@ exec { "apt-get update":
     command => "/usr/bin/apt-get update"
 }
 # Instalación del servidor de MySQL
-package { "mysql-server":
-    ensure => present,
-    require => Exec["apt-get update"]
-}
+#package { "mysql-server":
+#    ensure => present,
+#    require => Exec["apt-get update"]
+#}
  
 # Arrancar el servicio de MySQL
-service { "mysql":
-    ensure => running,
-    require => Package["mysql-server"]
-} 
+#service { "mysql":
+#    ensure => running,
+#    require => Package["mysql-server"]
+#} 
 
 # Editar password del usuario root de mysql
-exec { "set-mysql-root-password":
-    unless => "mysqladmin -u root -p1234 status",
-    command => "mysqladmin -u root password 1234",
-    require => Service["mysql"],
-    path => "/bin:/usr/bin",
+#exec { "set-mysql-root-password":
+#º    unless => "mysqladmin -u root -p1234 status",
+#   command => "mysqladmin -u root password 1234",
+#    require => Service["mysql"],
+#    path => "/bin:/usr/bin",
+#}
+$mysql_root_pw = '1234'
+
+class { '::mysql::server':
+      root_password => '1234',
+      restart => true,
+      override_options => {
+        mysqld => { bind-address => '0.0.0.0'} #Allow remote connections
+      },
+      # ... other class options
 }
-# Agregar usuario web (Recomiendo que si es produccion no sea root)
-exec { "set-mysql-user":
-    command => "mysql -uroot -p1234 -e\"CREATE USER 'root'@'192.168.50.4' IDENTIFIED BY '1234'\"; GRANT ALL on *.* TO 'root'@'192.168.50.4'; flush privileges;",
-    path => "/bin:/usr/bin",
+mysql_user{ 'root@%':
+        ensure  => present,
+        password_hash => mysql_password('1234'),
+        require  => Class['mysql::server'],
+      # ... other class options
 }
+mysql_grant{'root@%/mysql.*':
+        user  => 'root@%',
+        table => 'mysql.*',
+        privileges => ['ALL'],
+}
+
